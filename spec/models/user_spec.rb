4c681eb9c86fdcd163ee4e8bd75a5177d129b194
Abhidip Ray <aray@kreeti.com>
2014-09-03 18:02:40 +0530

    user model with secure passwords

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe User do
+  let!(:user) { User.new(name: "Abhidip", email: "aray@kreeti.com",
+                         password: "03324221898", password_confirmation: "03324221898") }
+  subject { user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:authenticate) }
+  it { should be_valid }
+
+  describe "name not present" do
+    before { user.name = "" }
+
+    it { should_not be_valid }
+  end
+
+  describe "email not present" do
+    before { user.email = "" }
+
+    it { should_not be_valid }
+  end
+
+  describe "name too long" do
+    before { user.name = user.name * 50 }
+
+    it { should_not be_valid }
+  end
+
+  describe "email format wrong" do
+    before { user.email = "abc@example" }
+
+    it { should_not be_valid }
+  end
+
+  describe "email has already been taken" do
+
+    before do
+      user.save!
+      @new_user = user.dup
+      @new_user.save
+    end
+
+    it "should have invalid duplicate email" do
+      expect(@new_user).not_to be_valid
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { user.password = user.password_confirmation = "A" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { user.save }
+    let(:found_user) { User.find_by(email: user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+
+    it "has blank password" do
+      expect(@user).not_to be_valid
+    end
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+end
